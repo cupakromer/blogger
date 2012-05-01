26a5187cb930885d98ef035c6771f30ba19169f6
Aaron Kromer <aaron.kromer@gmail.com>
2012-04-30 23:39:08 -0400

    Basic shell for Blogger

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '59be3035f9f5d30934645cbb79c85ce05d57e16bfe66b1ba8e2b37e1259287301d464e62679134042ca070d38637439d875d07f004492243ee13286e0cf699d5'
