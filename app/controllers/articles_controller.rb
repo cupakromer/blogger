9dcdf875c88aff77fb132fb39a68d8a20c026df5
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-01 22:17:20 -0400

    Added cosmetic fixes displaying time of comments and fixing bug with @article.comment.new interacting with @article.comments.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find params[:id]
-    @comment = @article.comments.new
+
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
