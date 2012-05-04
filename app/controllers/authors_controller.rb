98e6f690a09cb1115f7011ba53f00b95d49ef197
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-03 20:44:07 -0400

    Added some security so not just anyone can create an account.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,13 @@
 class AuthorsController < ApplicationController
-  # GET /authors
-  # GET /authors.json
+  before_filter :zero_authors_or_authenticated, only: [:new, :create]
+
+  def zero_authors_or_authenticated
+    unless Author.count == 0 || current_user
+      redirect_to root_path
+      return false
+    end
+  end
+
   def index
     @authors = Author.all
 
@@ -10,8 +17,6 @@ class AuthorsController < ApplicationController
     end
   end
 
-  # GET /authors/1
-  # GET /authors/1.json
   def show
     @author = Author.find(params[:id])
 
@@ -21,8 +26,6 @@ class AuthorsController < ApplicationController
     end
   end
 
-  # GET /authors/new
-  # GET /authors/new.json
   def new
     @author = Author.new
 
@@ -32,13 +35,10 @@ class AuthorsController < ApplicationController
     end
   end
 
-  # GET /authors/1/edit
   def edit
     @author = Author.find(params[:id])
   end
 
-  # POST /authors
-  # POST /authors.json
   def create
     @author = Author.new(params[:author])
 
@@ -53,8 +53,6 @@ class AuthorsController < ApplicationController
     end
   end
 
-  # PUT /authors/1
-  # PUT /authors/1.json
   def update
     @author = Author.find(params[:id])
 
@@ -69,8 +67,6 @@ class AuthorsController < ApplicationController
     end
   end
 
-  # DELETE /authors/1
-  # DELETE /authors/1.json
   def destroy
     @author = Author.find(params[:id])
     @author.destroy
