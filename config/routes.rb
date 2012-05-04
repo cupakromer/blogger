38bbcfb6dd90fd4658de3bd27bbeec318d48f9c0
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-03 20:33:29 -0400

    Added ability to login and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Blogger::Application.routes.draw do
   resources :authors
+  resources :author_sessions
+
+  match 'login'  => 'author_sessions#new',     as: :login
+  match 'logout' => 'author_sessions#destroy', as: :logout
 
   root to: 'articles#index'
   resources :articles
